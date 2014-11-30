92a202eac461052a8896fa3b5709bd984e7655c6
Casper Hansen <casper.hansen@outlook.com>
2014-11-30 21:57:44 +0100

    Clean up and polishing
    - Deleted a lot of files
    - Cleaned up code including comments
    - Added quote of the day
    - Added eventclick logic

--- js/calendar.js
+++ js/calendar.js
@@ -1,13 +1,18 @@
+/****************************************************
+Get all information before startup
+****************************************************/
+
 //Get all calendar & events
     getCalendars();
     getEvents();
-    getNotes();
     getUsers();
+    getQuote();
+    //getNotes();
 
     var calendars = [];
     var events = [];
-    var notes = [];
     var users = [];
+    //var notes = [];
 
     function getCalendars() {
 
@@ -43,10 +48,10 @@
             
             //Response from server
             request.done(function (response, textStatus, jqXHR){
-                
+                console.log(response);
                 response.forEach(function(event){
                     var eventObj = {};
-                    eventObj["id"] = event.eventid;
+                    eventObj["eventid"] = event.eventid;
                     eventObj["start"] = new Date(event.start);
                     eventObj["end"] = new Date(event.end);
                     eventObj["title"] = event.title;
@@ -58,8 +63,25 @@
                    
             });
     }
-    
-    function getNotes() {
+
+    function getQuote() {
+            
+        //Request to server
+            var request = $.ajax({
+                url: "http://127.0.0.1:52400/getQuote/",
+                type: "GET",
+                cache: false,
+                contentType: "application/json; charset=utf-8",
+                //data: header
+            });
+            
+            request.done(function (response, textStatus, jqXHR){
+                $('#qotdtopic').text(response.topic);
+                $('#qotd').text(response.quote);
+                $('#qotdauthor').text("Author: " + response.author);
+                
+                $(".QOTD").show();
+            });
     }
 
     function getUsers() {
@@ -83,12 +105,62 @@
             });
     }
 
+//Run when all Ajax-calls are done
 $(document).ajaxStop(function () {
     
+    //Populate calendar/userlists
     setCalendarList();
     setUserList();
     
-    //Create calendar
+    /****************************************************
+    Static utility object, for events data manipulation
+    ****************************************************/
+    var EventUtility = {
+          dateTimeasDigits: function (date) {
+              var date = new Date(date);
+              var hours = (date.getHours() < 10) ? "0" + date.getHours() : date.getHours();
+              var minutes = (date.getMinutes() < 10) ? "0" + date.getMinutes() : date.getMinutes();
+              return hours + ":" + minutes;
+              //return format: HH:mm
+          },
+          eventLocation: function (location) {
+              if (location.length > 25) {
+                  return location.substr(0, 25) + " ...";
+              } else {
+                  return location;
+              }
+          },
+          notes: function (notesObj) {
+              var lihtml = "";
+              notesObj.forEach(function (note) {
+                  lihtml += "<li>" + note.text + "</li>";
+              });
+              return (lihtml.length > 0) ? lihtml : "";
+          },
+          forecast: function (forecastData) {
+              if (forecastData != null) {
+                  return "<tr>" +
+                      "<td colspan=\"2\"><b>Vejret</b></td>" +
+                      "</tr>" +
+                      "<tr>" +
+                      "<td>Desc:</td>" +
+                      "<td>" + forecastData.desc + "</td>" +
+                      "</tr>" +
+                      "<tr>" +
+                      "<td>Temp:</td>" +
+                      "<td>" + forecastData.celsius + "</td>" +
+                      "</tr>"
+              } else {
+                  return "";
+              }
+
+          }
+
+      };
+    
+    /********************************************
+    Creation of FullCalender
+    ********************************************/
     $('#myCalendar').fullCalendar({
         
         //Calendar UI settings
@@ -108,16 +180,69 @@ $(document).ajaxStop(function () {
         eventBorderColor: '#bcbcbc',
         
         //Event is clicked
-        eventClick: function(calEvent, jsEvent, view) {
-        },
+        eventClick: function (calEvent, jsEvent, view) {
+                    $(this).popover({
+                        html: true,
+                        placement: "right",
+                        title: function () {
+                            return calEvent.title;
+                        },
+                        content: function () {
+                            return "<table class\"table\" width=\"100%\">" +
+                                "<tbody>" +
+                                "<tr>" +
+                                "<td><b> Tid: </b></td>" +
+                                "<td class=\"text-right\"> " + EventUtility.dateTimeasDigits(calEvent.start) + " - " + EventUtility.dateTimeasDigits(calEvent.end) + "</td>" +
+                                "</tr>" +
+                                "<tr>" +
+                                "<td><b> Lokale: </b></td>" +
+                                "<td class=\"text-right\"> " + EventUtility.eventLocation(calEvent.location) + " </td>" +
+                                "</tr>" +
+                                "<tr>" +
+                                "<td style=\"padding-top:10px;\" colspan=\"2\"><b> Noter</b></td>" +
+                                "</tr>" +
+                                "<tr>" +
+                                "<td colspan=\"2\">" +
+                                "<ul class=\"note-ul\">" +
+                                EventUtility.notes(calEvent.notes) +
+                                "<li><a href=\"#\" id=\"addNewNote" + calEvent._id + "\">Ny note</a></li>" +
+                                "</ul>" +
+                                "</td>" +
+                                "</tr>" +
+                                EventUtility.forecast(calEvent.forecast) +
+                                "</tbody" +
+                                "</table>";
+                        },
+                        container: "body"
+                    });
+                    $(this).popover('toggle');
+
+                    $.fn.editable.defaults.mode = 'inline';
+                    $('#addNewNote' + calEvent._id).editable({
+                        type: 'text'
+                    }).on("save", function (e, params) {
+
+                        var request = $.ajax({
+                            url: 'http://127.0.0.1:52400/',
+                            dataType: "json",
+                            type: "POST",
+                            data: "id=addNote&jsonData=" +
+                            "{\"userid\":'" + $.sessionStorage.get("userid") + "',\"eventid\":'" + calEvent.event_id + "',\"text\":'" + encodeURI(params.submitValue) + "'}"
+                        });
+
+                        request.done(function (response, textStatus, jqXHR) {
+
+
+                        });
+
+                    });
+            },
         
-        //Trigger create event
-        dayClick: function(date, jsEvent, view) {
-            
-            //Show modal
+        //Day is clicked
+        /*dayClick: function(date, jsEvent, view) {
+            //Trigger create event
             $('#eventModal').modal('show');
-            
-        },
+        },*/
     })
     
     //Log out button
@@ -127,17 +252,31 @@ $(document).ajaxStop(function () {
         window.location = 'login.html';
     });
     
-    //Create event button
+    /********************************************
+    Creation of event & call to server
+    ********************************************/
     $("#btnCreateEvent").click(function(){
             
+            var parseDate = function (date, datoSplit, tidSplit) {
+
+                    var dato = date.substr(0, date.indexOf(" "));
+                    var tid = date.substr(dato.length, date.length).trim();
+
+                    var d = dato.split(datoSplit);
+                    var t = tid.split(tidSplit);
+
+                    return d[2] + "-" + d[1] + "-" + d[0] + " " + t[0] + ":" + t[1] + ":00";
+
+                }
+        
             //Object of users input
             var event = {
                 userid : $.sessionStorage.get("userid"),
                 calendarid : $("#selectcalendar").find(":selected").val(),
                 title : $("#title").val(),
                 location : $("#location").val(),
-                start : $("#start").val(),
-                end : $("#end").val(),
+                start: parseDate($("#datetimepicker1").find("input").val(), "-", ":"),
+                end: parseDate($("#datetimepicker2").find("input").val(), "-", ":"),
             }
             
             //Data object the server will recieve
@@ -155,22 +294,26 @@ $(document).ajaxStop(function () {
             
             //Response from server
             request.done(function (response, textStatus, jqXHR){
-                //calResponse(response);
+                console.log(json);
+                calResponse(response);
             });
         
             //Check response from server
             function calResponse(response){
                 
-                if (response.response == "CALENDAR CREATED") {
-                    $('#myModal').modal('hide');     
+                if (response.response == "EVENT CREATED") {
+                    $('#eventModal').modal('hide');
+                    location.reload();
                 } else {
-                    $(".form-group").addClass("has-error");
+                    $(".form-group-event").addClass("has-error");
                 }
             }
         
     });
     
-    //Create Calendar button
+    /********************************************
+    Creation of calendar & call to server
+    ********************************************/
     $("#btnCreateCal").click(function(){
             
             //Object of users input
@@ -182,6 +325,7 @@ $(document).ajaxStop(function () {
             
             //Data object the server will recieve
             var json = "id=addCalendar&jsonData=" + JSON.stringify(calendar);
+            console.log(json);
 
             //Request to server
             var request = $.ajax({
@@ -202,27 +346,31 @@ $(document).ajaxStop(function () {
             function calResponse(response){
                 
                 if (response.response == "CALENDAR CREATED") {
-                    $('#calendarModal').modal('hide');     
+                    $('#calendarModal').modal('hide');
+                    location.reload();
                 } else {
-                    $(".form-group").addClass("has-error");
+                    $(".form-group-create").addClass("has-error");
                 }
             }
         
     });
     
-    $("#btnShareCalendar").click(function(){
+    /********************************************
+    Share calendar & call to server
+    ********************************************/
+    $("#btnShareCal").click(function(){
             
             //Object of users input
-            var calendar = {
-                calendarid : $("#selectcalendar").find(":selected").val(),
-                email : $("#selectcalendar").find(":selected").val(),
+            var calendarShare = {
+                calendarid : $("#sharecalendar").find(":selected").val(),
+                email : $("#shareuser").find(":selected").val(),
             }
             
             //Data object the server will recieve
-            var json = "id=addEvent&jsonData=" + JSON.stringify(calendar);
+            var json = "id=shareCalendar&jsonData=" + JSON.stringify(calendarShare);
             console.log(json);
 
-            /*//Request to server
+            //Request to server
             var request = $.ajax({
                 url: "http://127.0.0.1:52400/",
                 type: "POST",
@@ -233,21 +381,26 @@ $(document).ajaxStop(function () {
             
             //Response from server
             request.done(function (response, textStatus, jqXHR){
-                //calResponse(response);
-            });*/
+                console.log(response);
+                calResponse(response);
+            });
         
             //Check response from server
             function calResponse(response){
                 
-                if (response.response == "CALENDAR CREATED") {
-                    $('#myModal').modal('hide');     
+                if (response.response == "CALENDAR SHARED") {
+                    $('#shareModal').modal('hide');
+                    location.reload();
                 } else {
-                    $(".form-group").addClass("has-error");
+                    $(".form-group-share").addClass("has-error");
                 }
             }
         
     });
     
+    /********************************************
+    Delete calendar & call to server
+    ********************************************/
     $("#btnDeleteCal").click(function(){
             
             //Object of users input
@@ -270,16 +423,18 @@ $(document).ajaxStop(function () {
             
             //Response from server
             request.done(function (response, textStatus, jqXHR){
-                delResponse(response);
+                console.log(response);
+                calResponse(response);
             });
             
             //Check response from server
-            function delResponse(response){
+            function calResponse(response){
                 
                 if (response.response == "CALENDAR DELETED") {
-                    $('#deleteModal').modal('hide');     
+                    $('#deleteModal').modal('hide');
+                    location.reload();
                 } else {
-                    $(".form-group").addClass("has-error");
+                    $(".form-group-del").addClass("has-error");
                 }
             }
         
@@ -287,6 +442,10 @@ $(document).ajaxStop(function () {
     
 });
 
+
+/********************************************
+Populate calendar/userlists
+********************************************/
 function setCalendarList() {
         
         //Set calendars
